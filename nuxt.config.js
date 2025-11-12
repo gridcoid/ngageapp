8bbb2f0833a5c28cfa5a12fdf5932d4397e161d5
Andi Prianto <andi.prianto@gmail.com>
2025-11-12 08:27:52 +0700

    drop 'mode'

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,127 +1,126 @@
 export default {
   ssr: true,
-  mode: 'universal',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'UNIMIND',
+    title: "UNIMIND",
     meta: [
-      { charset: 'utf-8' },
-      { title: 'UNIMIND' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { title: "UNIMIND" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: 'Made with UNIMIND. Copyright Ⓒ 2023 Kompas Gramedia'
-      }
+        hid: "description",
+        name: "description",
+        content: "Made with UNIMIND. Copyright Ⓒ 2023 Kompas Gramedia",
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.svg' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/icon.svg" }],
   },
 
   publicRuntimeConfig: {
-    baseURL: process.env.BASE_URL || 'http://localhost:3000'
+    baseURL: process.env.BASE_URL || "http://localhost:3000",
   },
 
-  css: ['@/assets/css/main.scss'],
+  css: ["@/assets/css/main.scss"],
 
   layoutTransition: {
-    name: 'fade-transform'
+    name: "fade-transform",
   },
   pageTransition: {
-    name: 'fade-transform'
+    name: "fade-transform",
   },
   googleFonts: {
     families: {
-      Cabin: true
-    }
+      Cabin: true,
+    },
   },
 
   styleResources: {
-    scss: ['./assets/css/*.scss']
+    scss: ["./assets/css/*.scss"],
   },
 
   plugins: [
-    '~/plugins/notifier.js',
-    '~/plugins/repository.js',
-    '~/plugins/persistedState.client.js',
-    '~/plugins/datepicker.js',
-    '~/plugins/filterData.js',
-    { src: '~/plugins/chart.js', mode: 'client' },
-    { src: '~/plugins/select.js', mode: 'client' },
-    { src: '~/plugins/table.js', mode: 'client' },
-    { src: '~/plugins/jsonForm.js', mode: 'client' },
-    { src: '~/plugins/imageEditor.js', mode: 'client' },
-    { src: '~/plugins/password.js', mode: 'client' },
-    { src: '~/plugins/codemirror', ssr: false }
+    "~/plugins/notifier.js",
+    "~/plugins/repository.js",
+    "~/plugins/persistedState.client.js",
+    "~/plugins/datepicker.js",
+    "~/plugins/filterData.js",
+    { src: "~/plugins/chart.js", mode: "client" },
+    { src: "~/plugins/select.js", mode: "client" },
+    { src: "~/plugins/table.js", mode: "client" },
+    { src: "~/plugins/jsonForm.js", mode: "client" },
+    { src: "~/plugins/imageEditor.js", mode: "client" },
+    { src: "~/plugins/password.js", mode: "client" },
+    { src: "~/plugins/codemirror", ssr: false },
   ],
 
   components: true,
 
   buildModules: [
-    '@nuxtjs/google-fonts',
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/moment'
+    "@nuxtjs/google-fonts",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/moment",
   ],
 
   modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    '@nuxtjs/style-resources',
-    '@nuxt/content',
-    '@nuxtjs/auth-next',
-    '@nuxtjs/sentry'
+    "@nuxtjs/axios",
+    "@nuxtjs/pwa",
+    "@nuxtjs/style-resources",
+    "@nuxt/content",
+    "@nuxtjs/auth-next",
+    "@nuxtjs/sentry",
   ],
   sentry: {
-    dsn: 'https://ac3229f38bf6456e88f80dd4e00b8836@o4504495035580416.ingest.sentry.io/4504495039250432',
-    config: {}
+    dsn: "https://ac3229f38bf6456e88f80dd4e00b8836@o4504495035580416.ingest.sentry.io/4504495039250432",
+    config: {},
   },
   moment: {
     // locales: ['id']
   },
 
   axios: {
-    baseURL: process.env.BASE_URL || 'http://localhost:3000'
+    baseURL: process.env.BASE_URL || "http://localhost:3000",
   },
 
   router: {
-    middleware: ['auth']
+    middleware: ["auth"],
   },
 
   auth: {
     redirect: {
-      login: '/login',
-      logout: '/login',
-      callback: '/login',
-      home: false
+      login: "/login",
+      logout: "/login",
+      callback: "/login",
+      home: false,
     },
     strategies: {
       local: {
-        scheme: 'refresh',
+        scheme: "refresh",
         token: {
-          property: 'data.token',
+          property: "data.token",
           maxAge: 60 * 60 * 24 * 1,
-          global: true
+          global: true,
         },
         refreshToken: {
-          property: 'data.refreshToken',
-          data: 'requestToken',
-          maxAge: 60 * 60 * 24 * 1
+          property: "data.refreshToken",
+          data: "requestToken",
+          maxAge: 60 * 60 * 24 * 1,
         },
         endpoints: {
           login: {
-            url: 'auth/login',
-            method: 'post'
+            url: "auth/login",
+            method: "post",
           },
           refresh: {
-            url: 'auth/refresh',
-            method: 'post'
+            url: "auth/refresh",
+            method: "post",
           },
           logout: false,
-          user: false
+          user: false,
         },
-        autoLogout: true
-      }
+        autoLogout: true,
+      },
     },
-    plugins: ['~/plugins/auth.js']
+    plugins: ["~/plugins/auth.js"],
   },
 
   content: {},
@@ -130,5 +129,5 @@ export default {
 
   telemetry: false,
 
-  target: 'server'
-}
+  target: "server",
+};
