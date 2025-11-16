a339aaeb08742ec982991b221adc4e7db31ffb67
Andi Prianto <andi.prianto@gmail.com>
2025-11-16 16:07:28 +0700

    drop unused plugin

--- plugins/axios.js
+++
@@ -1,9 +0,0 @@
-// export default function ({ $axios, redirect, $auth }) {
-//   $axios.onError((error) => {
-//     console.log('error : ', error)
-//     // if (error.response.status === 401) {
-//     //   $auth.logout()
-//     //   redirect('/login')
-//     // }
-//   })
-// }
