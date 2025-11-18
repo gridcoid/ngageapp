c5e743cee617112506a726bfba2113460b9c2be7
Andi Prianto <andi.prianto@gmail.com>
2025-11-18 18:59:28 +0700

    register icon as global

--- plugins/icons.js
+++ plugins/icons.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
-import * as Icons from 'ico'  // imports your ico library
+import * as icons from 'ico' // your local dependency
 
-// Optional: register only in production to speed up dev
-if (process.env.NODE_ENV === 'production') {
-  Object.keys(Icons).forEach(name => {
-    Vue.component(name, Icons[name])
-  })
-}
+Object.entries(icons).forEach(([name, component]) => {
+  Vue.component(name, component)
+})
