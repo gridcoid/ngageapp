5ffb2bfa7bc90819193e3ac90814571f72b9fc5f
Andi Prianto <andi.prianto@gmail.com>
2025-12-16 19:08:02 +0700

    add dummy das

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,42 @@
-<!-- Dashboard -->
 <template>
-  <div>
-    <h1>Dashboard</h1>
+  <div class="kg-construction flex flex-col items-center justify-center">
+    <img src="~/assets/images/not_ready.png" width="150" />
+    <div class="title-cons">This page is not available yet.</div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  layout: 'default',
+  head() {
+    return {
+      title: 'Dashboard - ' + this.$config.appName,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.kg-construction {
+  height: 93vh;
+  width: 100%;
+  background-color: #fafbfc;
+}
+.title-cons {
+  font-style: normal;
+  font-weight: 600;
+  font-size: 20px;
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 5px;
+  color: #333333;
+}
+.sub-cons {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  text-align: center;
+  color: #757575;
+}
+</style>
