d15e4ff7864910347a567f8405e0a812cf887d62
Andi Prianto <andi.prianto@gmail.com>
2025-12-22 21:48:46 +0700

    update address info

--- pages/audience/index.vue
+++ pages/audience/index.vue
@@ -167,10 +167,13 @@
         <el-table-column label="Location">
           <template slot-scope="scope">
             <div class="k-subtitle leading-5">
-              {{ scope.row.village?.name || '-' }},
-              {{ scope.row.district?.name || '-' }}<br />
-              {{ scope.row.regency?.name || '-' }},
-              {{ scope.row.province?.name || '-' }}
+              <template v-if="hasLocation(scope.row)">
+                {{ scope.row.village?.name }}, {{ scope.row.district?.name
+                }}<br />
+                {{ scope.row.regency?.name }},
+                {{ scope.row.province?.name }}
+              </template>
+              <template v-else> - </template>
             </div>
           </template>
         </el-table-column>
@@ -401,6 +404,15 @@ export default {
     resetFilter() {
       this.radio = 'createdAt_desc'
       this.getData()
+    },
+
+    hasLocation(row) {
+      return (
+        row.village?.name ||
+        row.district?.name ||
+        row.regency?.name ||
+        row.province?.name
+      )
     },
   },
   watch: {
