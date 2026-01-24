8d17cec590c6f33782f87cc242cbea2eb054b6db
Andi Prianto <andi.prianto@gmail.com>
2026-01-24 10:03:08 +0700

    set timeout

--- repositories/sheetRepository.js
+++ repositories/sheetRepository.js
@@ -3,6 +3,8 @@ const resource = 'sheet'
 export default ($axios) => ({
   import(payload) {
     const orgId = window.$nuxt.$store.state.user.orgId
-    return $axios.post(`${resource}/import?orgId=${orgId}`, payload)
+    return $axios.post(`${resource}/import?orgId=${orgId}`, payload, {
+      timeout: 0, // ♾ no timeout
+    })
   },
 })
