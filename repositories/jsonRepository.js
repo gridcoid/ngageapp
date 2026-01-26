9dfcbccd7a27576f45099ab98cfc17c2cede2d2d
Andi Prianto <andi.prianto@gmail.com>
2026-01-26 22:38:02 +0700

    update json importer

--- repositories/jsonRepository.js
+++ repositories/jsonRepository.js
@@ -8,4 +8,10 @@ export default ($axios) => ({
       payload
     )
   },
+  import(payload) {
+    const orgId = window.$nuxt.$store.state.user.orgId
+    return $axios.post(`${resource}/import?orgId=${orgId}`, payload, {
+      timeout: 0, // ♾ no timeout
+    })
+  },
 })
--- store/json.js
+++ store/json.js
@@ -28,4 +28,21 @@ export const actions = {
       return e.response
     }
   },
+
+  async import({ commit }, payload) {
+    try {
+      const response = await this.$repositories.json.import(payload)
+      return response
+    } catch (e) {
+      this.$notifier.showMessage({
+        content:
+          'Error status code: ' +
+          e.response?.status +
+          ' ! ' +
+          e.response.data.data.message,
+        type: 'failed',
+      })
+      return e.response
+    }
+  },
 }
