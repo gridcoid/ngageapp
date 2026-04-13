06151f9fbd5026e4ee9b49807f13a947e5214683
Andi Prianto <andi.prianto@gmail.com>
2026-04-13 21:40:08 +0700

    unauthorized message

--- plugins/auth.js
+++ plugins/auth.js
@@ -2,9 +2,8 @@ export default function ({ store, $auth, $axios, redirect }) {
   $axios.onError((error) => {
     const code = parseInt(error.response && error.response.status)
     if (code === 401) {
-      $auth.logout()
-      store.dispatch('reset')
-      redirect('/login')
+      console.error('Unauthorized')
+      redirect('/')
     }
   })
 }
